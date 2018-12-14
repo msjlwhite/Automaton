cefeddabdfe44995d19518b2c39dd82e4ad14711
msjlwhite <msjlwhite@gmail.com>
2018-12-14 13:31:16 -0600

    Automaton Commit 1 -- 12.14.2018

---
+++ main.js
@@ -0,0 +1,44 @@
+/** NOTE
+ * J White
+ * 12.14.2018
+ * Automaton Delivery/Pick-up Project
+ * Goal -
+ *
+ */
+
+// MAP of Meadowfield that consists of 11 places with 14 roads between them.
+const roads =
+[
+    "Alice's House-Bob's House",   "Alice's House-Cabin",
+    "Alice's House-Post Office",   "Bob's House-Town Hall",
+    "Daria's House-Ernie's House", "Daria's House-Town Hall",
+    "Ernie's House-Grete's House", "Grete's House-Farm",
+    "Grete's House-Shop",          "Marketplace-Farm",
+    "Marketplace-Post Office",     "Marketplace-Shop",
+    "Marketplace-Town Hall",       "Shop-Town Hall"
+];
+
+
+//builds a graph of the locations (to --- from)
+function buildGraph(edges)
+{
+    let graph = Object.create(null);
+    function addEdge(from, to)
+    {
+      if (graph[from] == null)
+      {
+        graph[from] = [to];
+      }
+      else {
+        graph[from].push(to);
+      }
+    }
+    for (let [from, to] of edges.map(r => r.split("-")))
+    {
+      addEdge(from, to);
+      addEdge(to, from);
+    }
+    return graph;
+  }
+
+  const roadGraph = buildGraph(roads);
